a8ea87ccc8d8fda03db360617da91415bf7a8ac9
nachiketkallapur <nachiketkallapur@gmail.com>
2021-04-16 13:39:14 +0530

    First commit

---
+++ practice_program_1/input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("\nEnter x: ");
+    scanf("%d",&x);
+
+    printf("\n Value of x is: %d",x);
+
+    return 0;
+}
---
+++ practice_program_1/output.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    writef("\nEnter x: ");
+    readf("%d",&x);
+
+    writef("\n Value of x is: %d",x);
+
+    return 0;
+}
